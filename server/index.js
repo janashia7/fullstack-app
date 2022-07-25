2c60cdcd23031f1fc8498fe4bded2b36672c7071
Giorgi Janashia <gjanashia@unisens.ge>
2022-07-25 16:47:25 +0400

    setup simple server with express

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server listen to port ${PORT}`));
